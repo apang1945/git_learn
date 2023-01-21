a064075e36c6aa35ad040ef97fd674d88ee2027a
Mao <maotaotao1945@163.com>
2023-01-21 08:33:04 +0800

    add

--- tips/xiaoya-restart.sh
+++ tips/xiaoya-restart.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
-echo "重启xiaoya容器">> /root/taozi/restart.log
+echo "重启xiaoya容器">> /root/taozi/xiaoya/restart.log
+NOW=`date`
 docker restart xiaoya
-echo "重启命令的执行状态："$?>>echo "重启xiaoya容器">> /root/taozi/restart.log
+echo "重启命令的执行状态："$?>>echo "重启xiaoya容器">> /root/taozi/xiaoya/restart.log
 if [ $? -eq 0 ]; then
-    echo "执行成功！">> /root/taozi/restart.log
+    echo "执行成功！">> /root/taozi/xiaoya/restart.log
 else
-    echo "执行失败！">> /root/taozi/restart.log
+    echo "执行失败！">> /root/taozi/xiaoya/restart.log
     exit
 fi
-echo "重启xiaoya容器完毕！输出日志在/root/taozi/restart.log中">> /root/taozi/restart.log
+echo "重启xiaoya容器完毕！当前时间为：$NOW">> /root/taozi/xiaoya/restart.log
